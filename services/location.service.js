b002ea5cc607c886954af419742bf6d4a733d5cc
Nadav Nassi <nadav.nassi@gmail.com>
2022-08-08 12:27:24 +0300

    cleanup

--- services/location.service.js
+++ services/location.service.js
@@ -3,7 +3,7 @@ const {lookup} = require('geoip-lite')
 function getLocation(userIp){
     console.log("🚀 ~ file: location.service.js ~ line 4 ~ getLocation ~ userIp", userIp)
     const location = lookup(userIp)
-    console.log("🚀 ~ file: location.service.js ~ line 6 ~ getLocation ~ userIp", userIp)
+    console.log("🚀 ~ file: location.service.js ~ line 6 ~ getLocation ~ location", location)
     return location
 }
 
