40a4384da5cbc66f3e47e2a46ecb54fe1aa7350b
Nadav Nassi <nadav.nassi@gmail.com>
2022-08-08 12:26:52 +0300

    cleanup

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     console.log("🚀 ~ file: index.js ~ line 9 ~ app.get ~ ip", ip)
     // const {ip} = req.params
     const location = locationService.getLocation(ip)
-    res.send(JSON.stringify(location))
+    res.send(location)
 })
 
 const port = process.env.PORT || 3030
