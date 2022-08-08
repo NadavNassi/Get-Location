3f95a83c33f6bf2dbb686b6ce819d97ddc06005b
Nadav Nassi <nadav.nassi@gmail.com>
2022-08-08 12:41:09 +0300

    add params

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const locationService = require('./services/location.service')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-    console.log("🚀 ~ file: index.js ~ line 9 ~ app.get ~ ip", ip)
-    // const {ip} = req.params
+app.get('/:ip', (req, res) => {
+    const {ip} = req.params
     const location = locationService.getLocation(ip)
     res.send(location)
 })
--- services/location.service.js
+++ services/location.service.js
@@ -1,10 +1,7 @@
 const {lookup} = require('geoip-lite')
 
 function getLocation(userIp){
-    userIp = userIp.split(' ')[1]
-    console.log("🚀 ~ file: location.service.js ~ line 4 ~ getLocation ~ userIp", userIp)
     const location = lookup(userIp)
-    console.log("🚀 ~ file: location.service.js ~ line 6 ~ getLocation ~ location", location)
     return location
 }
 
